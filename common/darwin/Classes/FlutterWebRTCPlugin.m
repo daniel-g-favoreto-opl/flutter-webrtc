9d561b3bc796d766333f47e0074785647dda167b
CloudWebRTC <duanweiwei1982@gmail.com>
2025-01-24 14:01:01 +0800

    [iOS] Robustness for video renderer. (#1751)
    * Robustness for video renderer.
    
    * update.

--- common/darwin/Classes/FlutterWebRTCPlugin.m
+++ common/darwin/Classes/FlutterWebRTCPlugin.m
@@ -588,6 +588,10 @@ bypassVoiceProcessing:(BOOL)bypassVoiceProcessing {
       for (RTCVideoTrack* track in stream.videoTracks) {
         [_localTracks removeObjectForKey:track.trackId];
         RTCVideoTrack* videoTrack = (RTCVideoTrack*)track;
+        FlutterRTCVideoRenderer *renderer = [self findRendererByTrackId:videoTrack.trackId];
+        if(renderer != nil) {
+          renderer.videoTrack = nil;
+        }
         CapturerStopHandler stopHandler = self.videoCapturerStopHandlers[videoTrack.trackId];
         if (stopHandler) {
           shouldCallResult = NO;
@@ -702,6 +706,10 @@ bypassVoiceProcessing:(BOOL)bypassVoiceProcessing {
     if (audioTrack) {
       [self ensureAudioSession];
     }
+    FlutterRTCVideoRenderer *renderer = [self findRendererByTrackId:trackId];
+    if(renderer != nil) {
+      renderer.videoTrack = nil;
+    }
     result(nil);
   } else if ([@"restartIce" isEqualToString:call.method]) {
     NSDictionary* argsMap = call.arguments;
@@ -749,9 +757,11 @@ bypassVoiceProcessing:(BOOL)bypassVoiceProcessing {
     NSDictionary* argsMap = call.arguments;
     NSNumber* textureId = argsMap[@"textureId"];
     FlutterRTCVideoRenderer* render = self.renders[textureId];
-    render.videoTrack = nil;
-    [render dispose];
-    [self.renders removeObjectForKey:textureId];
+    if(render != nil) {
+      render.videoTrack = nil;
+      [render dispose];
+      [self.renders removeObjectForKey:textureId];
+    }
     result(nil);
   } else if ([@"videoRendererSetSrcObject" isEqualToString:call.method]) {
     NSDictionary* argsMap = call.arguments;
@@ -829,8 +839,10 @@ bypassVoiceProcessing:(BOOL)bypassVoiceProcessing {
       NSDictionary* argsMap = call.arguments;
       NSNumber* viewId = argsMap[@"viewId"];
       FlutterRTCVideoPlatformViewController* render = _platformViewFactory.renders[viewId];
-      render.videoTrack = nil;
-      [_platformViewFactory.renders removeObjectForKey:viewId];
+      if(render != nil) {
+        render.videoTrack = nil;
+        [_platformViewFactory.renders removeObjectForKey:viewId];
+      }
       result(nil);
     }
 #endif
@@ -2338,5 +2350,14 @@ bypassVoiceProcessing:(BOOL)bypassVoiceProcessing {
     @"receiver" : [self receiverToMap:transceiver.receiver]
   };
   return params;
+}
+
+- (FlutterRTCVideoRenderer *)findRendererByTrackId:(NSString *)trackId {
+    for (FlutterRTCVideoRenderer *renderer in self.renders.allValues) {
+        if (renderer.videoTrack != nil && [renderer.videoTrack.trackId isEqualToString:trackId]) {
+            return renderer;
+        }
+    }
+    return nil;
 }
 @end
